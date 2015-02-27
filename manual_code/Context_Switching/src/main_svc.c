11fa9bfcd95e573fc0e173a4d42e18fcacd6a65b
Rachel <rcmak@uwaterloo.ca>
2015-02-27 12:56:36 -0500

    added UART_IRQ for UART (keyboard) interrupt handling.

--- manual_code/Context_Switching/src/main_svc.c
+++ manual_code/Context_Switching/src/main_svc.c
@@ -30,7 +30,36 @@ int main()
 	
 	/* start the RTX and built-in processes */
 	rtx_init();  
-  
+	
+/*---------------intializes UART interrupts stuff.----------------------*/
+//   LPC_UART_TypeDef *pUart;
+// 	
+// 	__disable_irq();
+// 	
+// 	uart0_irq_init(); // uart0 interrupt driven, for RTX console
+// 	uart1_init();     // uart1 polling, for debugging
+// 	
+// 	#ifdef DEBUG_0
+// 		init_printf(NULL, putc);
+// 	#endif // DEBUG_0
+// 		__enable_irq();
+
+// 		uart1_put_string("COM1> Type a character at COM0 terminal\n\r");
+
+// 		pUart = (LPC_UART_TypeDef *) LPC_UART0;
+// 		
+// 		while( 1 ) {
+// 			
+// 			if (g_send_char == 0) {
+// 				/* Enable RBR, THRE is disabled */
+// 				pUart->IER = IER_RLS | IER_RBR;
+// 			} else if (g_send_char == 1) {
+// 				/* Enable THRE, RBR left as enabled */
+// 				pUart->IER = IER_THRE | IER_RLS | IER_RBR;
+// 			}
+// 			 
+// 		}
 	/* We should never reach here!!! */
 	return RTX_ERR;  
 }
+
